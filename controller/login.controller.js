8d0cf6e5495754fdc18523be32589639f0dea2ea
Nafisa Nawal <nafisanawal@iut-dhaka.edu>
2024-11-08 02:47:51 +0600

    ensured middleware for user login

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -1,10 +1,10 @@
 const db = require('../db');
 
-const handleLogin = async(req, res) => {
+const handleLogin = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // Find user by email and password
+        
         const user = await db.user.findFirst({
             where: {
                 email,
@@ -16,8 +16,10 @@ const handleLogin = async(req, res) => {
             return res.status(400).send('Invalid email or password.');
         }
 
+        req.session.isAuthenticated = true;
         req.session.email = user.email;
-        res.redirect('/dashboard');
+
+        res.redirect('/dashboard'); 
     } catch (error) {
         console.error(error);
         res.status(500).send('An error occurred during login.');
