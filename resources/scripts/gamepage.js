32a46f8523c5dfc2785d36402c231c0c5ff57b65
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2025-03-19 03:49:32 +0600

    bug fixing

---
+++ resources/scripts/gamepage.js
@@ -0,0 +1,20 @@
+
+fetch('/dashboard/profile/data')
+.then(response => response.json())
+.then(data => {
+    document.getElementById('welcomeText').innerText = `Welcome, ${data.name}!`;
+})
+.catch(err => console.error(err));
+
+const userIcon = document.getElementById('userIcon');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+userIcon.addEventListener('click', () => {
+dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+});
+
+document.addEventListener('click', (event) => {
+if (!userIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+    dropdownMenu.style.display = 'none';
+}
+});
